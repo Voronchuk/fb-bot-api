c6d0309e721903283b957b93af688e5a98a4461b
AlexMold <mangol72@gmail.com>
2016-05-14 18:10:48 +0300

    set test env

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const Facebook = require('../src/Facebook');
+const request = require('request-promise');
+const assert = require('assert');
+const fs = require('fs');
+
+describe('Facebook Bot', function(){
+
+})
